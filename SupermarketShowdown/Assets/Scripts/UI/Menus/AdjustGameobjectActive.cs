f6f5185651a3edb3285f6dca0b2ebc80a39fc8c7
Wesley Elmer <wesley.elmer@mymail.champlain.edu>
2021-01-31 17:09:24 -0500

    Implementing Main Menu and Creating Pause Menu
    Main menu fully implemented (without animation.)
    Pause menu exists and is fully functional, but needs to be integrated into the UI manager.

---
+++ SupermarketShowdown/Assets/Scripts/UI/Menus/AdjustGameobjectActive.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustGameobjectActive : MonoBehaviour
+{
+    public GameObject targetObject;
+
+    public void SetGameObjectActive(bool state)
+    {
+        targetObject.SetActive(state);
+    }
+}
