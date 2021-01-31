b226c869ecbd8224fc77435f0b1c6cb728193e3b
Wesley Elmer <wesley.elmer@mymail.champlain.edu>
2021-01-31 10:30:00 -0500

    Creating main menu

---
+++ SupermarketShowdown/Assets/Scripts/UI/Menus/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Author:      Wesley Elmer
+// Description: Provides a scene transition for buttons
+public class ChangeScene : MonoBehaviour
+{
+    public string targetScene = "SampleScene";
+    
+    public void Load()
+    {
+        SceneManager.LoadScene(targetScene);
+    }
+}
